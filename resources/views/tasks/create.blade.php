fe0c42459322fcd984797bee1286835f6c723da6
ayaka.kitano <ayaka.kitano@rakuten.com>
2018-06-01 07:52:03 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,17 +3,22 @@
 @section('content')
 
 <h1>タスク新規登録ページ</h1>
-
+<div class="row">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
         
+        <div class="form-group">
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
         {!! Form::label('status' , 'ステータス:') !!}
-        {!! Form::text('status')!!}
+        {!! Form::text('status', null, ['class' => 'form-control'])!!}
+        </div>
 
-        {!! Form::submit('登録') !!}
+        {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
-
+    </div>
+</div>
 @endsection
