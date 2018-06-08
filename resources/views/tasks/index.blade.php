693e8853b25bbeab1433bc53137099046297af21
ayaka.kitano <ayaka.kitano@rakuten.com>
2018-06-08 04:38:07 +0000

    fix4

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -17,7 +17,7 @@
                   @foreach ($tasks as $task)
                     <tr>
                         <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                        <td>{{ $task->content }}</td>
+                        <td>{{ $task->content}}</td>
                         <td>{{ $task->status }}</td>
                     </tr>
                 @endforeach
